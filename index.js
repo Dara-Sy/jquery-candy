7c7196058df32ce527791cae32322e452259657d
Dara-Sy <darajanne@gmail.com>
2018-07-30 23:49:20 -0400

    stuff

---
+++ index.js
@@ -0,0 +1,22 @@
+$("ul").html();
+    // output will be
+    // <li>Skittles</li>
+    // <li>Starburst</li>
+    // <li>Kit Kat</li>
+
+// changes the attributes
+$("img").attr({
+  "width" : "500px",
+  "height" : "300px"
+});
+
+// will show your candy choice in console once selected
+$("select").val();
+
+$("h1").addClass("correct");
+
+$("h1").removeClass("correct");
+
+$('option').addClass("wrong");
+
+$(".or").toggleClass("done");
